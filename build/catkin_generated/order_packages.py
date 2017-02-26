07821412b690d7e8dc2c1086d0b37fd74f0b1484
BlueCrescent <ick40195+ros@gmail.com>
2017-02-26 13:04:47 +0900

    delete build directory

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/ubuntu/Documents/catkin_ws/src/minimum_ros_control_moveit/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/ubuntu/Documents/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/ubuntu/Documents/catkin_ws/devel;/opt/ros/kinetic" != "" else []
