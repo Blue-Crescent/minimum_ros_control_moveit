f9df936ad0c59712b524f5bd46e16d93c27dbed7
ubuntu <ick40195+ros@gmail.com>
2017-03-18 23:05:51 +0900

    update leaf link

--- src/trobo_control/src/trobo_hw.cpp
+++ src/trobo_control/src/trobo_hw.cpp
@@ -9,13 +9,21 @@
 TRobo::TRobo()
 {
   // connect and register the joint state interface
-  hardware_interface::JointStateHandle state_handle_1("joint1", pos_, vel_, eff_);
+  hardware_interface::JointStateHandle state_handle_1("joint1", &pos_[0], &vel_[0], &eff_[0]);
   jnt_state_interface.registerHandle(state_handle_1);
+
+  // connect and register the joint state interface
+  hardware_interface::JointStateHandle state_handle_2("joint2", &pos_[1], &vel_[1], &eff_[1]);
+  jnt_state_interface.registerHandle(state_handle_2);
   registerInterface(&jnt_state_interface);
 
   // connect and register the joint position interface
-  hardware_interface::JointHandle pos_handle_1(jnt_state_interface.getHandle("joint1"), cmd_);
+  hardware_interface::JointHandle pos_handle_1(jnt_state_interface.getHandle("joint1"), &cmd_[0]);
   jnt_pos_interface.registerHandle(pos_handle_1);
+  
+  // connect and register the joint position interface
+  hardware_interface::JointHandle pos_handle_2(jnt_state_interface.getHandle("joint2"), &cmd_[1]);
+  jnt_pos_interface.registerHandle(pos_handle_2);
   registerInterface(&jnt_pos_interface);
 }
 
@@ -29,8 +37,9 @@ void TRobo::write(ros::Time time, ros::Duration period)
   // Real Robot functionality coding here...
   // below code is simulating real robot delay.	
   pos_[0] = pos_[0] + 0.0001* (cmd_[0] - pos_[0]);
+  pos_[1] = pos_[1] + 0.0001* (cmd_[1] - pos_[1]);
   ROS_DEBUG_STREAM("Debug:" << pos_[0] << cmd_[0]);
   // Dump cmd_ from MoveIt!, current simulated real robot pos_.
-  printf("%f, %f\n",pos_[0],cmd_[0]);
+  //printf("%f, %f\n",pos_[0],cmd_[0]);
 }
 
